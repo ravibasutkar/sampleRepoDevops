18e0fe84c8c700e39410206b61f894a7604bc463
ravisb <ravisb@LIN65005889.corp.capgemini.com>
2022-10-13 11:25:28 +0530

    1st commit to per sec

---
+++ src/devopssession2.java
@@ -0,0 +1,9 @@
+
+public class devopssession2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  
+		//this is for second repository SampleRepoDEVOPS 3rd day
+
+	}
+
+}
